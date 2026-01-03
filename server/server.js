f390d22a703fb59eeff692ab7b25334230baf197
VISHALX33 <vishalprajapati2303@gmail.com>
2026-01-03 10:36:06 +0530

    server

--- server/server.js
+++ server/server.js
@@ -5,35 +5,33 @@ import app from './app.js'
 import connectDB from './config/database.js'
 import socketHandler from './socket/socketHandler.js'
 
-// Load environment variables
 dotenv.config()
 
-// Connect to database
 connectDB()
 
-// Create HTTP server
+if (!process.env.CLIENT_URL) {
+  throw new Error('CLIENT_URL is not defined');
+}
+
 const httpServer = createServer(app)
 
-// Socket.io setup
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ['GET', 'POST'],
   },
 })
 
-// Socket.io connection
 socketHandler(io)
 
-// Start server
 const PORT = process.env.PORT || 5000
 
 httpServer.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-  console.log(`API URL: http://localhost:${PORT}/api`)
 })
 
-// Handle unhandled promise rejections
+// Safety
 process.on('unhandledRejection', (err) => {
   console.error(`Error: ${err.message}`)
   httpServer.close(() => process.exit(1))
